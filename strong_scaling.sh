2e80dc19d99bdb75935b9c92bc39c06080d79c30
Pierre-Louis Lefebvre <pierre-louis.lefebvre@insa-lyon.fr>
2019-01-18 11:53:59 +0100

    iterations

---
+++ strong_scaling.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# makes $2 iterations of one execution and get the mean time
+# $1 : the name of the command to execute
+# $2 : the iterations count
+# $3 : the name of the output file
+function iter_exec {
+
+    EXEC=$1
+    ITERATIONS=$2
+    OUTPUT_FILE=$3
+
+    timeSum=0;
+
+    OLDIFS="$IFS"
+    IFS=","
+
+    echo "Temps d'execution de $EXEC" > ${OUTPUT_FILE}
+
+    for ((i=0 ; ITERATIONS - $i ; i++))
+    do
+        "./${EXEC}" |tail -n1 |read execTime
+        let "timeSum=$timeSum + $execTime"
+        echo "$execTime" >> ${OUTPUT_FILE}
+    done
+
+    IFS="$OLDIFS"
+
+    echo "Moyenne : " >> ${OUTPUT_FILE}
+
+    echo "scale=1;$timeSum/$ITERATIONS" | bc | tr -d "\n" >> ${OUTPUT_FILE}
+}
+
+iter_exec $1 $2 $3
+
+#    CORES=$1
+#    LENGTH=$2
