74afb74b588f523f0d886db7b338a7fcfa44a84c
L P <poisse.lucas@gmail.com>
2019-01-25 09:51:12 +0100

    ADD devops

---
+++ devops/git_compile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# checkout git branch and compile the executable
+# $1 : the name of the branch
+# $2 : the compilation options (CXX_FLAGS)
+
+BRANCH_NAME=$1
+COMPILE_OPTIONS=$2
+
+git fetch
+git stash
+git stash clear
+git checkout -f ${BRANCH_NAME}
+
+rm -f CMakeCache.txt
+
+cd ..
+cmake . -DCMAKE_CXX_FLAGS="${COMPILE_OPTIONS}"
+make
+cd devops
