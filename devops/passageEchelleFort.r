832e53f3cb9d4cafc2b126f5a6f77161d24c9ad5
L P <poisse.lucas@gmail.com>
2019-01-25 08:14:35 +0100

    MAJ R/CSV

--- devops/passageEchelleFort.r
+++ devops/passageEchelleFort.r
@@ -19,14 +19,9 @@ for (i in 1:length(argsL)){
     perfData <- read.csv(file=(argsL)$file, header=TRUE, sep=",")
   }
 }
-taillesGrille <- unique(select(perfData, taille_cote_grille))
+taillesGrille <- unique(select(perfData, version))
 print(taillesGrille)
-for(i in 1:nrow(taillesGrille)){
-  print("taille=")
-  print(taillesGrille[i,])
-  perfDataFiltered <- perfData[perfData$taille_cote_grille ==  taillesGrille[i,],]
-  print(perfDataFiltered)
-  perfPlot <- ggplot(perfDataFiltered, aes(x = nb_threads, y=tempsMoyen))+ geom_col() + scale_x_log10(breaks = trans_breaks("log2", function(x) 2^x),
-                                                                                              labels = trans_format("log2", math_format(2^.x)))
-  ggsave(paste("PassageEchelleFort-", taillesGrille[i,], "x", taillesGrille[i,], ".png"))
-}
+ggplot(perfData, aes(x = nb_threads, y=tempsMoyen, group=version, colour=version))+ geom_line()+
+    geom_point() + scale_x_log10(breaks = trans_breaks("log2", function(x) 2^x),labels = trans_format("log2", math_format(2^.x)))
+
+ggsave(paste("PassageEchelleFort.png"))
