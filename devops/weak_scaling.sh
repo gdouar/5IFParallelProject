e33e17f90e9c9af5283d18cc828a872d2612bb73
Pierre-Louis Lefebvre <pierre-louis.lefebvre@insa-lyon.fr>
2019-01-25 15:11:59 +0100

    testing weak_scaling

--- devops/weak_scaling.sh
+++ devops/weak_scaling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Make a strong scaling by successive powers of 2 on the actual compiled version of pdc_mini_aevol
+# Make a weak scaling by successive powers of 2 on the actual compiled version of pdc_mini_aevol
 # $1 : The name of the optimisation
 # $2 : The name of the global CSV file for the results of the strong scaling, scheme :
 # "opt_name,nb_threads,t1,t2,...,mean_time"
@@ -8,19 +8,32 @@
 # $4 : The number of iterations of one type of execution
 # $5 : The number of generations in one execution
 # $6 : The maximum scaling (must be power of 2)
+# $7 : The side start size of the population grid, which is a square
 OPT_NAME="$1"
 GLOBAL_CSV="$2"
 RESULTS_DIR="$3"
 ITERATIONS=$4
 GENERATIONS=$5
 MAX_SCALING=$6
+START_SIDE=$7
 
 GLOBAL_CSV_FULLNAME="${RESULTS_DIR}${GLOBAL_CSV}"
+
+height=${START_SIDE}
+width=${START_SIDE}
+height_growth=0
 
 for (( i=1 ; $MAX_SCALING>=$i ; i=$i * 2 ))
 do
     echo "executions with ${i} threads"
     printf "${OPT_NAME},${i}," >> ${GLOBAL_CSV_FULLNAME}
-    ./iter_exec.sh "../pdc_mini_aevol -n ${GENERATIONS} --threads ${i}" "${ITERATIONS}" "${GLOBAL_CSV_FULLNAME}"
+    ./iter_exec.sh "../pdc_mini_aevol -n ${GENERATIONS} -w ${width} -h ${height} --threads ${i}" "${ITERATIONS}" "${GLOBAL_CSV_FULLNAME}"
+
+    if [[ ${height_growth} == 1 ]]
+    then
+        (( height*=2 ))
+    else
+        (( width*=2 ))
+    fi
 done
 
